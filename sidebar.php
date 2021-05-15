2db1f042c3a606a54627e9a69612a1fe5eb74b6e
Sachika Baba <baba@professy.co.jp>
2021-05-16 00:43:54 +0900

    fixed w3p #35

--- sidebar.php
+++ sidebar.php
@@ -1,38 +1,37 @@
 <aside class="col-md-4">
     <div class="sidebar">
-    <h3 class="sidebar-title">人気の記事</h3>
         <section class="topArticles">
-        
-        <?php
-        $i = 1;
-        $query = new WP_query( array(
-            'posts_per_page' => 3,
-            'meta_key' => 'post_views_count', // アクセス数ごとに並び替える
-            'orderby'   => 'meta_value_num' // アクセス数ごとに並び替える
-        ));
-        if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
-        ?>
+            <h3 class="sidebar-title">人気の記事</h3>
+            <?php
+            $i = 1;
+            $query = new WP_query( array(
+                'posts_per_page' => 3,
+                'meta_key' => 'post_views_count', // アクセス数ごとに並び替える
+                'orderby'   => 'meta_value_num' // アクセス数ごとに並び替える
+            ));
+            if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+            ?>
 
-            <article class="topArticles-element" itemscope itemtype ="https://schema.org/BlogPosting" class="article-item">
-                <span class="topArticles-element__num"><?php echo ($i); $i++; ?></span>
-                <h4 class="topArticles-element__title" itemprop="headline"><a href="<?php the_permalink(); ?>" itemprop="mainEntityOfPage"><?php the_title(); ?></a></h4>
-            </article>
+                <article class="topArticles-element" itemscope itemtype ="https://schema.org/BlogPosting" class="article-item">
+                    <span class="topArticles-element__num"><?php echo ($i); $i++; ?></span>
+                    <h4 class="topArticles-element__title" itemprop="headline"><a href="<?php the_permalink(); ?>" itemprop="mainEntityOfPage"><?php the_title(); ?></a></h4>
+                </article>
 
-        <?php
-        endwhile; 
-        endif;
-        wp_reset_query();
-        ?>
+            <?php
+            endwhile; 
+            endif;
+            wp_reset_query();
+            ?>
         </section>
     </div>
 
-    <div class="sidebar">
-        <div class="profile" itemprop="author" itemscope itemtype="https://schema.org/Person">
+    <div class="sidebar" itemscope itemtype="https://schema.org/Person">
+        <div class="profile" itemprop="author">
         <h3 class="sidebar-title" itemprop="name"><?php the_author_meta('nickname'); ?></h3>
             <img class="profile-image" src="<?php echo get_avatar_url(wp_get_current_user()); ?>" alt="プロフィール画像" >
         <div class="profile-text">
         <p><?php the_author_meta('description'); ?></p>
         </div>
     </div>
-    </div>
+
 </aside>
