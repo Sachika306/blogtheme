7416fad6424bfb3fd2a9a0e614a554f55b3e93c4
Sachika306 <sachikababa306@gmail.com>
2023-10-15 17:27:46 +0900

    test

--- batches/create-ranking.php
+++ batches/create-ranking.php
@@ -3,81 +3,92 @@
  * 必須情報設定
  * 注意：このファイルの名前を変えたら、wp-cli.ymlの設定も変える必要がある
  --------------------------------------*/
+//  require '../include/vendor/symfony/autoload.php';
+ use Google\Analytics\Data\V1beta\BetaAnalyticsDataClient;
+ use Google\Analytics\Data\V1beta\DateRange;
+ use Google\Analytics\Data\V1beta\Dimension;
+ use Google\Analytics\Data\V1beta\Metric;
+ use Google\Analytics\Data\V1beta\OrderBy;
+
 class createRanking extends WP_CLI_Command {
+
 	function createRankingByWeeklyPageView() {
-		// APIを叩くのに必要な設定が入ってるか確認
-		if (
-			!get_theme_mod('gpc_service_account_name') ||
-			!file_exists(dirname(__DIR__, 1)  . '/keys/' . get_theme_mod('gpc_service_account_key')) ||
-			!get_theme_mod('google_analytics_id') 
-		) {
-			throw new Error('APIの実行に必要情報がサイドバーから登録されていないか、キーが見つかりませんでした');
-		}
-		
+
 		try {
-			define('SERVICE_ACOUNT_NAME', get_theme_mod('gpc_service_account_name')); // サービスアカウント名（メールアドレス）, GCPで確認できる。
-			define('KEY_FILE_LOCATION', dirname(__DIR__, 1)  . '/keys/' . get_theme_mod('gpc_service_account_key')); // キーファイルのパス TODO:外部から呼び出し
-			define('ANALYTICS_VIEW_ID','ga:' . get_theme_mod('google_analytics_id')); // アナリティクスのビューID 例)'ga:1234567' TODO
+			putenv('GOOGLE_APPLICATION_CREDENTIALS='  . dirname(__DIR__, 1)  . '/keys/' . get_theme_mod('gpc_service_account_key'));
 			define('MAX_GET_COUNT', 10); // APIで取得したい件数、固定で10件取得。
-			define('START_DATE', date("Y-m-d", strtotime("-1 week"))); // APIで取得したい日の対象A（A日～）
-			define('END_DATE', date("Y-m-d", strtotime("-1 day"))); // APIで取得したい日の対象B（～B日まで）
 
-			// APIで取得する条件
-			$target_array = array(
-				'dimensions'  => 'ga:pagePath', // 取得するディメンション
-				'sort'        => '-ga:pageviews', // ページビューを取得
-				'max-results' => MAX_GET_COUNT, //件数
-				'filters' => 'ga:pagePath=~/?p=;ga:pagePath!@wp-admin' // フィルタリングが必要な場合はこちらに、例えば特定ディレクトリ直下なら（~/FujisakiShiori/）など記述
-			);
+			$client = new BetaAnalyticsDataClient();
 
-			// PHP用ライブラリ
-			$phpLibrary = dirname(__DIR__, 1) . '/include/vendor/autoload.php';
-			require_once $phpLibrary;
+			$response = $client->runReport([
+				'property' => 'properties/' . get_theme_mod('google_analytics_id'),
+				'dateRanges' => [
+					new DateRange([
+						'start_date' => '8daysAgo',
+						'end_date' => '1daysAgo',
+					]),
+				],
+			
+				'dimensions' => [
+					new Dimension(
+						[
+							'name' => 'pagepath',
+						]
+					),
+				],
+			
+				'metrics' => [
+					new Metric(
+						[
+							'name' => 'sessions',
+						]
+					),
+				],
 
-			/**
-			 * Analytics Apifunction Client生成
-			 */
-			$client = new Google_Client();
+				'limit' => MAX_GET_COUNT,
 
-			// トークンのセット
-			if (isset($_SESSION['service_token'])) {
-				$client->setAccessToken($_SESSION['service_token']);
+			]);
+
+			$result = array();
+			foreach ($response->getRows() as $row) {
+				$dimensionValues = array();
+				foreach($row->getDimensionValues() as $v) {
+					$dimensionValues[] = $v->getValue();
+				}
+			
+				$metricValues = array();
+				foreach ($row->getMetricValues() as $v) {
+					$metricValues[] = $v->getValue();
+				}
+			
+				// パスのスラッシュ取る
+				$dimensionValue = str_replace("/", "", $dimensionValues[0]);
+
+				// ホーム画面の結果は不要
+				if (empty($dimensionValue)) {
+					echo "aaa";
+				} else {
+					$result[] = array($dimensionValue, $metricValues[0]);
+				}
+			}
+			
+			if (!isset($result)) {
+				throw new Error('ランキング取得結果のファイルへの書き込みがうまくいきませんでした');
 			}
 
-			// クレデンシャルの作成
-			$client->setAuthConfig(KEY_FILE_LOCATION);
-			$client->useApplicationDefaultCredentials();
-			$client->addScope('https://www.googleapis.com/auth/analytics');
+			var_dump($result);
 
-			// セッションの設定
-			$_SESSION['service_token'] = $client->getAccessToken();
-
-			// アナリティクスクライアントを生成
-			$analytics = new Google_Service_Analytics($client);
-
-			//指定期間のPVランキング取得
-			$result = $analytics->data_ga->get(
-				ANALYTICS_VIEW_ID,
-				START_DATE, //開始日
-				END_DATE, //終了日
-				'ga:pageviews',
-				$target_array
-			);
-
-			print_r($result['rows']);
-		
 			$fileOutputPath = get_theme_file_path("/batchOutput/ranking-result.php");
-			$fileOutputResult = file_put_contents($fileOutputPath, json_encode($result['rows']));
+			$fileOutputResult = file_put_contents($fileOutputPath, json_encode($result));
 			
 			if (!$fileOutputResult) {
 				throw new Error('ランキング取得結果のファイルへの書き込みがうまくいきませんでした');
 			}
 			
 		} catch (Exception $e) {
-			echo '例外：',  $e->getMessage(), "\n";
-		}
+			echo $e->getMessage(), "\n";
+		};
 	}
 }
-
 WP_CLI::add_command('create-ranking', 'createRanking');
 ?>
--- template-parts/showRanking.php
+++ template-parts/showRanking.php
@@ -2,13 +2,15 @@
     $rankingResultPath = get_theme_file_path('batchOutput/ranking-result.php');
     $rankingResult = wp_json_file_decode($rankingResultPath, true);
     $maxPostNum = (get_theme_mod('popular_article_control'));
+    var_dump($rankingResult);
     for ($i=0; $i<=$maxPostNum; $i++) :
         $postId = preg_replace('/[^0-9]/', '', $rankingResult[$i][0]);
+        var_dump(get_page_by_path(($rankingResult[$i][0])));
 ?>
     <article class="topArticles-element" itemscope itemtype ="https://schema.org/BlogPosting">
         <span class="topArticles-element__num"><?php echo ($i+1); ?></span>
         <h4 class="topArticles-element__title" itemprop="headline">
-            <a href="<?php echo get_the_permalink($postId); ?>" itemprop="mainEntityOfPage">
+            <a href="<?php echo get_permalink( get_page_by_path($postId) ); ?>" itemprop="mainEntityOfPage">
                 <?php echo get_the_title($postId); ?>
             </a>
         </h4>
